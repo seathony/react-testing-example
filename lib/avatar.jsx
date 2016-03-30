af4fec887238d0c3f348946b2871314ca2bac45e
sean <seathony@gmail.com>
2016-03-30 19:12:43 -0400

    avatar test passing

---
+++ lib/avatar.jsx
@@ -0,0 +1,17 @@
+import React, { PropTypes } from 'react';
+
+export default function Avatar(props) {
+  return (
+    <div className="avatar">
+      <p>
+        <em>{props.email}</em>
+      </p>
+      <img src={props.src} className="img-rounded" />
+    </div>
+  );
+}
+
+Avatar.propTypes = {
+  email: PropTypes.string.isRequired,
+  src: PropTypes.string.isRequired,
+};
